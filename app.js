5d039141ebc27596c7bba5f17a8490febe8af853
Barry <barrycumbie@gmail.com>
2024-10-10 13:35:16 -0500

    full crud on fed

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.use(express.static('./public/'))
 
-console.log(uri);
-
-console.log('im on a node server change that and that tanad f, yo');
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,37 +21,18 @@ const client = new MongoClient(uri, {
   }
 });
 
-// function whateverNameOfIt (params) {}
-// ()=>{}
-
-app.get('/', function (req, res) {
-  // res.send('Hello Node from Ex on local dev box')
-  res.sendFile('index.html');
-})
-
-app.get('/ejs', (req,res)=>{
-``
-  res.render('index', {
-    myServerVariable : "something from server"
-  });
-
-  //can you get content from client...to console? 
-})
-
-app.get('/read', async (req,res)=>{
-
-  console.log('in /read');
-  await client.connect();
+app.get('/', async function (req, res) {
   
-  console.log('connected?');
-  // Send a ping to confirm a successful connection
+  console.log('in /');
+  await client.connect();  
+  console.log('I should be connected');
   
   let result = await client.db("barrys-db").collection("whatever-collection")
     .find({}).toArray(); 
-  console.log(result); 
+  // console.log(result); 
 
-  res.render('read', {
-    postData : result
+  res.render('name', {
+    nameData : result
   });
 
 })
@@ -64,47 +41,39 @@ app.post('/insert', async (req,res)=> {
 
   console.log('in /insert');
   
-  console.log('request', req.body);
-  console.log('request', req.body.newPost);
-
   //connect to db,
   await client.connect();
   //point to the collection 
-  await client.db("barrys-db").collection("whatever-collection").insertOne({ post: req.body.newPost});
-  // await client.db("barrys-db").collection("whatever-collection").insertOne({ iJustMadeThisUp: 'hardcoded new key '});  
-  //insert into it
-  res.redirect('read');
+  await client.db("barrys-db").collection("whatever-collection").insertOne({ fname: req.body.fname});
+  res.redirect('/');
 
 }); 
 
-app.post('/update/:id', async (req,res)=>{
+app.post('/update', async (req,res)=>{
 
-  console.log("req.parms.id: ", req.params.id)
+  console.log("req.body: ", req.body)
 
   client.connect; 
   const collection = client.db("barrys-db").collection("whatever-collection");
   let result = await collection.findOneAndUpdate( 
-  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
+  {"_id": new ObjectId(req.body.nameID)}, { $set: {"fname": req.body.inputUpdateName } }
 )
 .then(result => {
   console.log(result); 
-  res.redirect('/read');
+  res.redirect('/');
 })
 }); 
 
 app.post('/delete/:id', async (req,res)=>{
 
-  console.log("req.parms.id: ", req.params.id)
+  console.log("in delete, req.parms.id: ", req.params.id)
 
   client.connect; 
   const collection = client.db("barrys-db").collection("whatever-collection");
   let result = await collection.findOneAndDelete( 
-  {"_id": new ObjectId(req.params.id)})
-
-.then(result => {
+  {"_id": new ObjectId(req.params.id)}).then(result => {
   console.log(result); 
-  res.redirect('/read');
-})
+  res.redirect('/');})
 
   //insert into it
 
---
+++ public/scripts/name-script.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() { 
+  
+
+  // $('#inputUpdateName').on('focus', ()=>{
+
+  //   console.log('here'); 
+  //   if($('#inputUpdateName').prop( "disabled"))
+  //   {
+  //    $('#inputUpdateName').prop( "disabled", false)
+  //   }
+     
+
+
+  // })
+
+ 
+
+
+//   $('#btnUserName').click(function(e) {
+    
+//     e.preventDefault(); 
+//     console.log("button clicked")
+// //     var x; 
+// //     var x = ""; //string
+// //     var y = 8; 
+// //     var z = TRUE; 
+    
+//     let fname = $('#fname').val()
+     
+//     console.log(fname);
+//       // $('#fname').val()
+       
+//     let greeting; 
+    
+//     greeting = "Hi There, " + fname + "! What's Up?"
+    
+//     $('#something').text(greeting);
+    
+
+//   }); 
+
+ 
+  
+});
